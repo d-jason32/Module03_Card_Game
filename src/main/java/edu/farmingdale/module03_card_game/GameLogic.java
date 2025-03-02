dadaed7343f41da7d8ba11da2c2305f4a064e6b1
Jason Devaraj <jdevaraj01@gmail.com>
2025-03-01 19:52:38 -0500

    Added generate 4 random cards method.

--- src/main/java/edu/farmingdale/module03_card_game/GameLogic.java
+++ src/main/java/edu/farmingdale/module03_card_game/GameLogic.java
@@ -2,7 +2,13 @@ package edu.farmingdale.module03_card_game;
 
 public class GameLogic {
 
-    public void generateRandomCards(){
+    public static int[] generateRandomCards(){
+        int[] arr = new int[4];
 
+        for (int i = 0; i < 4; i++) {
+            int randomNumber = (int) (Math.random() * 52) + 1;
+            arr[i] = randomNumber;
+        }
+        return arr;
     }
 }
